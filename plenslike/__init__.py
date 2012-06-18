74db60234d20a4100204809a8578dd9425abbf27
Duncan Hanson <dhanson@jpl.nasa.gov>
2012-06-17 23:07:16 -0700

    tidy up. rename basic->mono. add first set of v50 mono data files. add routines to for renormalization of the mono likelihood.

--- plenslike/__init__.py
+++ plenslike/__init__.py
@@ -1,7 +1,11 @@
 import os
 import ctypes as ct
 
-class plenslike_dat_basic(ct.Structure):
+from _plenslike import *
+
+datadir = os.path.dirname(__file__) + "/data/"
+
+class plenslike_dat_mono(ct.Structure):
     _fields_ = [ ("nbins",         ct.c_int),
                  ("lmax",          ct.c_int),
                  ("bin_lmins",     ct.POINTER(ct.c_int)),
@@ -11,19 +15,38 @@ class plenslike_dat_basic(ct.Structure):
                  ("mat_sigma_inv", ct.POINTER(ct.c_double)),
                  ("clpp_fid",      ct.POINTER(ct.c_double)),
                  ("cltt_fid",      ct.POINTER(ct.c_double)),
-                 ("vl_inv",        ct.POINTER(ct.c_double)),
-                 ("al_inv",        ct.POINTER(ct.c_double)),
+                 ("bl_fid",        ct.POINTER(ct.c_double)),
                  ("fl",            ct.POINTER(ct.c_double)),
-                 ("bl",            ct.POINTER(ct.c_double)) ]
+                 ("vl_inv",        ct.POINTER(ct.c_double)),
+                 ("al_inv",        ct.POINTER(ct.c_double)) ]
 
-plenslike_dat_basic_pointer = ct.POINTER(plenslike_dat_basic)
 pll = ct.CDLL( os.path.dirname(__file__) + "/_plenslike.so")
-pll.load_plenslike_dat_basic.restype = plenslike_dat_basic_pointer
-pll.calc_plenslike_basic.restype     = ct.c_double
+pll.load_plenslike_dat_mono.argtypes   = [ ct.POINTER(plenslike_dat_mono), ct.c_char_p]
+pll.free_plenslike_dat_mono.argtypes   = [ ct.POINTER(plenslike_dat_mono) ]
+pll.calc_plenslike_mono.restype        = ct.c_double
+pll.calc_plenslike_mono_renorm.restype = ct.c_double
 
-class basic():
+class mono():
     def __init__(self, tfname):
-        self.dat = pll.load_plenslike_dat_basic(tfname)
+        print "plenslike:: loading mono likelihood from ", tfname
+
+        self.dat = plenslike_dat_mono()
+        pll.load_plenslike_dat_mono( ct.byref(self.dat), tfname)
 
     def __del__(self):
-        pll.free_plenslike_dat_basic(self.dat)
+        pll.free_plenslike_dat_mono(self.dat)
+
+    def calc_like(self, clpp):
+        assert( len(clpp) >= self.dat.lmax )
+        return pll.calc_plenslike_mono( ct.byref(self.dat),
+                                        clpp.ctypes.data_as( ct.POINTER(ct.c_double) ) )
+
+    def calc_like_renorm(self, clpp, cltt, bl):
+        assert( len(clpp) >= self.dat.lmax )
+        assert( len(cltt) >= self.dat.lmax )
+        assert( len(bl)   >= self.dat.lmax )
+
+        return pll.calc_plenslike_mono_renorm( ct.byref(self.dat),
+                                               clpp.ctypes.data_as( ct.POINTER(ct.c_double) ),
+                                               cltt.ctypes.data_as( ct.POINTER(ct.c_double) ),
+                                               bl.ctypes.data_as(   ct.POINTER(ct.c_double) ) )
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import glob
 def configuration(parent_package='',top_path=None):
     from numpy.distutils.misc_util import Configuration
     config = Configuration('plenslike',parent_package,top_path)
-    config.add_extension('_plenslike', ['plenslike/plenslike.c'], undef_macros=['NDEBUG'])
+    config.add_extension('_plenslike', ['plenslike/plenslike.c', 'plenslike/wignerd.c', 'plenslike/plenslike.pyf'], undef_macros=['NDEBUG'])
     return config
 
 if __name__ == "__main__":
@@ -13,5 +13,6 @@ if __name__ == "__main__":
 
     setup(name='plenslike',
           packages=['plenslike'],
-          package_data={'plenslike': glob.glob("data/*")},
+          package_data={'plenslike':
+                        [tf.replace("plenslike/", "") for tf in glob.glob('plenslike/data/v50/plenslike_v50_mono_???*.dat')] },
           configuration=configuration)
